96dbee2a30ec949db591ae347ceec8d31d5b7627
Steven Hosking <shosking@vigilant.it>
2018-08-29 18:33:09 +1000

    added create BIOS boot partition

--- WTGOfflineBoot.ps1
+++ WTGOfflineBoot.ps1
@@ -1,4 +1,4 @@
-#region config
+﻿#region config
 $apFilePath = "$env:SystemDrive\tempAP"
 $apFile = "$apFilePath\$($env:ComputerName).csv"
 $aadSecGroup = "ZZZ - VIT Intune Devices"
@@ -74,7 +74,7 @@ foreach ($ap in $apConnect) {
     Add-AzureADGroupMember -ObjectId $grp.objectid -RefObjectId $device.objectid
 }
 $ErrorActionPreference = "stop"
-
+$uefi = $true
 $disk = get-disk | Where-Object {$_.isboot -notlike $True}
 $disk | Set-Disk -IsOffline $False
 $disk | set-disk -IsReadOnly $False
@@ -84,30 +84,32 @@ if ($disk.PartitionStyle -eq "MBR") {
     $sysPar = New-Partition -DiskNumber $disk.DiskNumber -UseMaximumSize -MbrType IFS -IsActive -AssignDriveLetter
     $drvLtr = $sysPar.DriveLetter
     $sysVol = Format-Volume -Partition $sysPar -FileSystem NTFS -Force -Confirm:$False
-    
+    $uefi = $False
 }
 elseif ($disk.PartitionStyle -eq "GPT") {
     Clear-Disk -Number $disk.DiskNumber -RemoveData -Confirm:$False -RemoveOEM
     Initialize-Disk -Number $disk.DiskNumber -PartitionStyle GPT
-    
     $systemPartition = New-Partition -DiskNumber $disk.Number -Size 260MB -GptType '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}' -AssignDriveLetter
-
     $systemVolume = Format-Volume -Partition $systemPartition -FileSystem FAT32 -Force -Confirm:$False
-
     $systemPartition | Set-Partition -GptType '{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}'
     $systemPartition | Add-PartitionAccessPath -AssignDriveLetter
     $windowsPartition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -GptType '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}' -AssignDriveLetter
-
     $windowsVolume = Format-Volume -Partition $windowsPartition -FileSystem NTFS -Force -Confirm:$False
     $drvLtr = $windowsVolume.DriveLetter
+    $uefi = $False
 }
 $isoPath = "C:\en_windows_10_business_editions_version_1803_updated_march_2018_x64_dvd_12063333.iso"
 $ISOdisk = Mount-DiskImage $isoPath -PassThru
 $isoLtr = (Get-DiskImage -ImagePath $isoPath | Get-Volume).DriveLetter
 Import-Module dism
 Expand-WindowsImage -ApplyPath "$($drvLtr)`:" -ImagePath "$($isoLtr):\sources\install.wim" -Index 3
-
+if($uefi){
 $bcdBootArgs = "$drvLtr`:\windows /s $($systemPartition.driveletter)`: /v"
+}
+else {
+    $bcdBootArgs = "$drvLtr`:\windows /s $($systemPartition.driveletter)`: /v /f BIOS"
+}
+
 Start-Process "bcdboot.exe" -ArgumentList " $bcdBootArgs" -Wait
 
 $disk | set-disk -isreadonly $True
