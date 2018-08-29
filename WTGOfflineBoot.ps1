8e0964bac40fd161ff83214e391ca63893e8f838
Ben Reader <breader@vigilant.it>
2018-08-29 10:28:24 +1000

    FORMATTING

--- WTGOfflineBoot.ps1
+++ WTGOfflineBoot.ps1
@@ -1,11 +1,19 @@
 ﻿$apfilepath = "c:\tempap"
 $apfile = "$apfilepath\$($env:computername).csv"
 $aadsecgroup = "AAD AP Group"
-if (!(Test-Path -Path $apfilepath)) {new-item -Path $apfilepath -ItemType Directory}
+if (!(Test-Path -Path $apfilepath)) {
+    new-item -Path $apfilepath -ItemType Directory
+}
 $nugetver = (Get-PackageProvider -name "NuGet").version
-if ($nugetver.major -lt 2 -and $nugetver.Minor -lt 8) {Install-PackageProvider -name "nuget" -ForceBootstrap -Force | Out-Null}
-if ((Get-InstalledScript -Name "get-windowsautopilotinfo").version -lt 1.3) {Install-Script -Name "get-windowsautopilotinfo" -Force}
-if ((Get-ExecutionPolicy).ToString() -ne "Bypass") {Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope CurrentUser}
+if ($nugetver.major -lt 2 -and $nugetver.Minor -lt 8) {
+    Install-PackageProvider -name "nuget" -ForceBootstrap -Force | Out-Null
+}
+if ((Get-InstalledScript -Name "get-windowsautopilotinfo").version -lt 1.3) {
+    Install-Script -Name "get-windowsautopilotinfo" -Force
+}
+if ((Get-ExecutionPolicy).ToString() -ne "Bypass") {
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope CurrentUser
+}
 get-windowsautopilotinfo.ps1 -OutputFile $apfile
 
 if ((get-module -listavailable -name AzureADPreview).count -ne 1) {
@@ -28,15 +36,15 @@ Connect-AutoPilotIntune -user $adminuser | Out-Null
 Import-AutoPilotCSV -csvFile $apfile
 $grp = get-azureadgroup -SearchString $aadsecgroup
 $choice = "Wait to be added to group?"
-while ($choice -notmatch "[y|n]"){
+while ($choice -notmatch "[y|n]") {
     $choice = read-host "Do you want to continue? (Y/N)"
-    }
-    if ($choice -eq "y"){
-foreach ($ap in $apcontent) {
-    while ((Get-AzureADDevice -SearchString $ap.'Device Serial Number').count -ne 1) {Start-Sleep -Seconds 10}
-    $device = Get-AzureADDevice -SearchString $ap.'Device Serial Number'
-    Add-AzureADGroupMember -ObjectId $grp.objectid -RefObjectId $device.objectid
 }
+if ($choice -eq "y") {
+    foreach ($ap in $apcontent) {
+        while ((Get-AzureADDevice -SearchString $ap.'Device Serial Number').count -ne 1) {Start-Sleep -Seconds 10}
+        $device = Get-AzureADDevice -SearchString $ap.'Device Serial Number'
+        Add-AzureADGroupMember -ObjectId $grp.objectid -RefObjectId $device.objectid
+    }
 }
 $ErrorActionPreference = "stop"
 
@@ -53,7 +61,9 @@ if ($disk.PartitionStyle -eq "MBR") {
 }
 elseif ($disk.PartitionStyle -eq "GPT") {
     $OSpar = $disk | Get-Partition | Sort-Object Size -Descending | Select-Object -First 1
-    if ($ospar.size -lt 64Gb) {$ospar = $disk | New-Partition -UseMaximumSize -AssignDriveLetter}
+    if ($ospar.size -lt 64Gb) {
+        $ospar = $disk | New-Partition -UseMaximumSize -AssignDriveLetter
+    }
     $drvltr = $OSpar.DriveLetter
     Format-Volume -DriveLetter $drvltr -FileSystem NTFS
 }
@@ -64,7 +74,6 @@ Import-Module dism
 Expand-WindowsImage -ApplyPath "$($drvltr)`:" -ImagePath "$($isoltr):\sources\install.wim" -Index 3
 if (!(test-path -Path "$($drvltr):\efi\microsoft\boot\bcd")) {
     $bcdbootargs = "$($drvltr):\windows /s $drvltr`: /v /f UEFI"
-        
     Start-Process "bcdboot.exe" -ArgumentList " $bcdbootargs" -Wait
     Start-Process "bcdboot.exe" -ArgumentList "/store $($drvltr):\efi\microsoft\boot\bcd /set `{bootmgr`} device locate" -Wait
     Start-Process "bcdboot.exe" -ArgumentList "/store $($drvltr):\efi\microsoft\boot\bcd /set `{default`} device locate" -Wait
